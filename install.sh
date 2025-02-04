84173ebb848fa81f5a0cd9a18a5975f8ea7333da
jagratigoyal <dghybrisdev@nagarro.com>
2025-02-04 23:19:27 +0530

    config commit

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
+aws s3 cp s3://codepipeline-us-east-1-289254585277/target/testproject.war/sample-java-projects/testproject.war /tmp
+sudo mv /tmp/testproject.war /usr/share/tomcat/webapps/testproject.war
 sudo service tomcat restart
